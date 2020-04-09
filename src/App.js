689830adc2361795224862aa28bdd8f1958c3f6c
Samuel Julkunen <swjulkunen@gmail.com>
2020-04-09 14:20:08 -0600

    added a toggle feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import StoreList from './components/items/StoreList';
 import ListForm from './components/items/ListForm';
+import { Button, Icon} from 'semantic-ui-react';
 
 class App extends Component {
 state = { groceries: [
@@ -8,7 +9,12 @@ state = { groceries: [
   {id:2, items: 'pizza' , price: '10'},
   {id:3, items: 'cereal', price: '7'},
 
-]}
+],
+showForm: true 
+
+}
+
+toggleForm = () => this.setState({ showForm: !this.state.showForm})
 
 deleteList = (id) => {
 const groceries = this.state.groceries.filter( list => {
@@ -29,13 +35,18 @@ let newList= { id: this.getId(), ...incomingList }
 this.setState({ groceries: [  newList, ...this.state.groceries]})
 }
   render() {
-    const {groceries} = this.state
+    const { groceries, showForm } = this.state
     return(
       
       <div>
-       
         <h1>Grocery Store List </h1>
-        <ListForm addList={this.addList} />
+        <Button onClick={this.toggleForm}>
+          Show Form Click <Icon name= 'angle double down' />
+        </Button>
+        {
+            showForm ? <ListForm addList={this.addList} /> : null  
+        }
+
         <StoreList groceries={groceries} deleteList = {this.deleteList}/>
       </div>
     )
